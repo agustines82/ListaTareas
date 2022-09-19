3ac9e6031f55033d6c4fdd5aedbad94f50c84ae4
Agustin Sampayo <agustines82@gmail.com>
2022-09-19 18:47:49 -0300

    maquetado del formulario de tareas

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Form, Button } from "react-bootstrap";
 
 const FormularioTarea = () => {
     return (
-        <div>
-            <p>aqui va el formulario</p>
-        </div>
+        <Form className="my-3">
+            <Form.Group className="mb-1">
+                <Form.Control type="text" placeholder="Describe tu tarea" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Agregar Tarea
+            </Button>
+        </Form>
     );
 };
 
