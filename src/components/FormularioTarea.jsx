6e491490390c1d35f0485e1212dffbc2decafff2
Agustin Sampayo <agustines82@gmail.com>
2022-09-21 16:39:03 -0300

    agregamos boton eliminar y flexbox en el item de la lista

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -10,7 +10,7 @@ const FormularioTarea = () => {
             <Form className="mb-3 lead w-75 container border rounded-bottom">
                 <Form.Group>
                     <InputGroup>
-                        <Form.Control className="mb-4 mt-3" placeholder="Ingrese una tarea" aria-label="tarea" aria-describedby="tarea" />
+                        <Form.Control type="text" className="mb-4 mt-3" placeholder="Ingrese una tarea" />
                         <Button className="mb-4 mt-3" type="submit" variant="primary" id="btnTarea">
                             Enviar
                         </Button>
--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import { ListGroup } from "react-bootstrap";
+import { ListGroup, Button } from "react-bootstrap";
 const ItemTarea = () => {
-    return <ListGroup.Item className="lead bg-light rounded ">Buscar a Nacho del cole</ListGroup.Item>;
+    return (
+        <ListGroup.Item className="lead bg-light rounded d-flex justify-content-between ">
+            Buscar a Nacho del cole
+            <Button variant="danger">Borrar</Button>
+        </ListGroup.Item>
+    );
 };
 
 export default ItemTarea;
