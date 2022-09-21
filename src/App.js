86ead740ec14a9b06189362cb5cf71c88f419947
Agustin Sampayo <agustines82@gmail.com>
2022-09-21 17:21:21 -0300

    guardamos las tareas en el arreglo de tareas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import FormularioTarea from "./components/FormularioTarea";
-import ListaTareas from "./components/ListaTareas";
+
 //importamos la hoja de estilo para todo el proyecto en app.js
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
@@ -11,11 +11,6 @@ function App() {
                 <h1 className="display-4 text-center">Lista de tareas</h1>
                 <hr />
                 <FormularioTarea />
-                <section className="w-75 container">
-                    <article className="p-1">
-                        <ListaTareas />
-                    </article>
-                </section>
             </Container>
         </>
     );
--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,22 +1,46 @@
 import React from "react";
+import { useState } from "react";
 import { Form, Button, InputGroup } from "react-bootstrap";
+import ListaTareas from "./ListaTareas";
 
 const FormularioTarea = () => {
+    const [tarea, setTarea] = useState("");
+    const [arregloTareas, setArregloTareas] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // no puedo usar el push en un state por que no puedo modificar directamente la variable para ello necesito del operador spread ...
+        setArregloTareas([...arregloTareas, tarea]);
+        //limpiar el input (previamente se puso el value en el imput)
+        setTarea("");
+    };
+
     return (
         <section>
             <div className="container w-75 bg-light border rounded-top">
                 <Form.Label className="lead ms-2 mt-1">Nueva Tarea</Form.Label>
             </div>
-            <Form className="mb-3 lead w-75 container border rounded-bottom">
+            <Form className="mb-3 lead w-75 container border rounded-bottom" onSubmit={handleSubmit}>
                 <Form.Group>
                     <InputGroup>
-                        <Form.Control type="text" className="mb-4 mt-3" placeholder="Ingrese una tarea" />
+                        <Form.Control
+                            type="text"
+                            className="mb-4 mt-3"
+                            placeholder="Ingrese una tarea"
+                            onChange={(e) => setTarea(e.target.value)}
+                            value={tarea}
+                        />
                         <Button className="mb-4 mt-3" type="submit" variant="primary" id="btnTarea">
                             Enviar
                         </Button>
                     </InputGroup>
                 </Form.Group>
             </Form>
+            <section className="w-75 container">
+                <article className="p-1">
+                    <ListaTareas />
+                </article>
+            </section>
         </section>
     );
 };
